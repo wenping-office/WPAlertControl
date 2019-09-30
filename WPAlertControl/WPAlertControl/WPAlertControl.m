7458740cec8004227f269e3fcee0ac0d8eca8265
Developer <850500722@qq.com>
2019-09-30 10:12:26 +0800

    【target】适配iOS13

--- WPAlertControl/WPAlertControl/WPAlertControl.m
+++ WPAlertControl/WPAlertControl/WPAlertControl.m
@@ -237,6 +237,7 @@
         [self setModalPresentationStyle:UIModalPresentationOverCurrentContext];
         self.view.clipsToBounds = YES;
         UIButton *mask = [UIButton new];
+        self.modalPresentationStyle = UIModalPresentationFullScreen;
         [mask addTarget:self action:@selector(maskTouch:) forControlEvents:UIControlEventTouchUpInside];
         _mask = mask;
         [self.view addSubview:mask];
